e3d504d2d6b3d5272b258eaad077b509400c1374
Michael Klein <michael@firstiwaslike.com>
2015-01-06 21:14:02 +0100

    Include s3-adapter from ember-deploy project.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* jshint node: true */
 'use strict';
 
+var S3Adapter = require('./lib/s3');
+
 module.exports = {
-  name: 'ember-deploy-s3'
-};
+  name: 'ember-deploy-s3',
+
+  type: 'ember-deploy-addon',
+
+  adapters: {
+    assets: {
+      's3': S3Adapter
+    }
+  }
+}
